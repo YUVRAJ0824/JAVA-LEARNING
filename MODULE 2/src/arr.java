64f263523a41118a4b9d9719392ca6ce833f6709
YUVRAJ V <yraj3342@gmail.com>
2026-09-15 00:22:10 +0530

    movin slow

--- MODULE 2/src/arr.java
+++ MODULE 2/src/arr.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
-
+//ln40 imp for create
+// display value of array ln22-35
 public class arr {
     public static  void main(String[] args)
     {
@@ -28,6 +29,27 @@ public class arr {
    Value: 1
    Value: 2
    Value: 3 ... */
+
+//double of array
+        double[] timestamp={1.1,1.2,1.3,1.4,1.5};
+        System.out.println(Arrays.toString(timestamp));
+
+
+        //1. Provide all values at once (Java sets the size automatically)
+
+
+        int[] sets = {1, 2, 3, 4};
+        // Automatically makes a size-4 array
+        //2. Set the size first, then add values one by one using the index
+        System.out.println(Arrays.toString(sets));
+
+        int[] set = new int[4]; // Makes a size-4 array filled with zeros
+
+        set[0] = 1;  // Puts 1 in the first box
+        set[1] = 2;  // Puts 2 in the second box
+        set[2] = 3;
+        set[3] = 10;
+        System.out.println(Arrays.toString(set));
 
 
     }
